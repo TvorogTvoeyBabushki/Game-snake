a23c0fa7acd6142045e4e85b7932ca321864a588
Grimes <snake3773@mail.ru>
2023-03-11 16:41:59 +0300

    feat: add eating apples and passing through walls

--- js/snake.js
+++ js/snake.js
@@ -9,10 +9,11 @@ food.src = './img/apply.png'
 
 const box = 32
 
+let count = 0
 let score = 0
 let dir
 
-const apply = {
+let apply = {
     x: Math.floor((Math.random() * 17) + 1) * box,
     y: Math.floor((Math.random() * 15) + 3) * box
 }
@@ -20,41 +21,82 @@ const apply = {
 const snake = []
 snake[0] = {
     x: 9 * box,
-    y: 10 * box
+    y: 10 * box,
+    dx: box, 
+    dy: 0
 }
 
 function drawGame() {
+    requestAnimationFrame(drawGame)
+
+    if (++count < 4) {
+        return;
+    }
+
+    count = 0;
+
     context.drawImage(background, 0, 0)
 
     context.drawImage(food, apply.x, apply.y)
 
-    snake.forEach((item, index, arr) => {
-        let snakeX = item.x
-        let snakeY = item.y
-    
-        arr.pop()
-    
-        if(dir === 'left') snakeX -= box
-        if(dir === 'right') snakeX += box
-        if(dir === 'up') snakeY -= box
-        if(dir === 'down') snakeY += box
-
-        if(snakeX === 0) snakeX = 17 * box
-    
-        const newHead = {
-            x: snakeX,
-            y: snakeY
-        }
-    
-        arr.unshift(newHead)
-
-        context.fillStyle = 'green'
-        context.fillRect(item.x, item.y, box, box)
+    snake.forEach((item, index) => {
+        context.fillStyle = (index === 0) ? 'red' : 'green'
+        context.fillRect(item.x, item.y, box - 1, box - 1)
     })
+
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+
+    if (snakeX === apply.x && snakeY === apply.y) {
+        score++
+        apply = {
+            x: Math.floor((Math.random() * 17) + 1) * box,
+            y: Math.floor((Math.random() * 15) + 3) * box
+        }
+    } else {
+        snake.pop()
+    }
+
+    if (dir === 'left') snakeX -= box
+    if (dir === 'right') snakeX += box
+    if (dir === 'up') snakeY -= box
+    if (dir === 'down') snakeY += box
+
+    if (snakeX < box) {
+        snakeX = canvas.width - box * 2
+    } else if (snakeX > canvas.width - box * 2) {
+        snakeX = box
+    } else if (snakeY < 3 * box) {
+        snakeY = canvas.height - box * 2
+    } else if (snakeY > canvas.height - box * 2) {
+        snakeY = 3 * box
+    }
+
+    const newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    eatTail(newHead, snake)
+
+    snake.unshift(newHead)
 
     context.fillStyle = 'white'
     context.font = '50px Arial'
     context.fillText(score, box * 2.5, box * 1.7)
+}
+
+function eatTail(head, arr) {
+    arr.forEach(item => {
+        if (head.x === item.x && head.y === item.y) {
+            snake.x = 9 * box
+            snake.y = 10 * box
+            apply = {
+                x: Math.floor((Math.random() * 17) + 1) * box,
+                y: Math.floor((Math.random() * 15) + 3) * box
+            }
+        }
+    })
 }
 
 document.addEventListener('keydown', (event) => {
@@ -69,4 +111,8 @@ document.addEventListener('keydown', (event) => {
     }
 })
 
-let game = setInterval(drawGame, 100)
+let game = setInterval(requestAnimationFrame(drawGame), 100)
+
+// 1. не работает сброс при столкновение змейки со своим телом
+// 2. нужно уменьшить скорость змейки
+// 3. 
