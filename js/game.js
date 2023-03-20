52cc829abe53ee835ce1092bca30a91ba65a273b
foxta1ll <snake3773@mail.ru>
2023-03-20 13:19:37 +0300

    feat: add OOP

--- js/game.js
+++ js/game.js
@@ -11,5 +11,34 @@ class Canvas {
         this.context.drawImage(this.background, 0, 0)
     }
 }
-const canvas = new Canvas()
-let game = setInterval(canvas, 100)
+
+class Apply {
+    constructor(canvas) {
+        this.x = 0
+        this.y = 0
+        this.canvas = canvas
+
+        this.food = new Image()
+        this.food.src = './img/apply.png'
+    }
+
+    draw(context) {
+        context.drawImage(this.food, this.x, this.y)
+    }
+}
+
+class Game {
+    constructor() {
+        this.canvas = new Canvas()
+        this.apply = new Apply(this.canvas)
+    }
+
+    draw() {
+        this.canvas.draw.bind(canvas)
+        this.apply.draw(this.canvas.context)
+    }
+}
+
+const game = new Game()
+
+let gameLoop = setInterval(game.draw, 100)
