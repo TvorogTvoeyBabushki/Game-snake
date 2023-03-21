4be5e830f3a1cac35a239f576635022797ccf44e
foxta1ll <snake3773@mail.ru>
2023-03-21 16:54:01 +0300

    refactor: change structure of the OOP

--- js/game.js
+++ js/game.js
@@ -12,32 +12,92 @@ class Canvas {
     }
 }
 
+class Config {
+    constructor() {
+        this.box = 32
+        this.speedGame = 1
+    }
+}
+
 class Apply {
-    constructor(canvas) {
+    constructor() {
         this.x = 0
         this.y = 0
-        this.canvas = canvas
+        this.canvas = canvas.context
+        this.config = config.box
 
         this.food = new Image()
         this.food.src = './img/apply.png'
+
+        this.randomPositionApply()
     }
 
-    draw(context) {
-        context.drawImage(this.food, this.x, this.y)
+    draw() {
+        this.canvas.drawImage(this.food, this.x, this.y)
+    }
+
+    randomPositionApply() {
+        this.x = Math.floor((Math.random() * 17) + 1) * this.config
+        this.y = Math.floor((Math.random() * 15) + 3) * this.config
+    }
+}
+
+class Snake {
+    constructor() {
+        this.x = 8 * 32
+        this.y = 10 * 32
+        this.dx = 32
+        this.dy = 0
+        this.tails = []
+        this.maxTails = 3
+
+        this.canvas = canvas.context
+    }
+
+    updata() {
+        this.x += this.dx
+        this.y += this.dy
+
+        this.tails.unshift({
+            x: this.x,
+            y: this.y
+        })
+    
+        if (this.tails.length > this.maxTails) {
+            this.tails.pop()
+        }
+    }   
+
+    draw() {
+        this.tails.forEach((item, index) => {
+            if (index === 0) {
+                this.canvas.fillStyle = 'red'
+            } else {
+                this.canvas.fillStyle = 'green'
+            }
+            this.canvas.fillRect(item.x, item.y, 32 - 1, 32 - 1)
+        })
     }
 }
 
 class Game {
     constructor() {
-        this.canvas = new Canvas()
-        this.apply = new Apply(this.canvas)
+    }
+
+    updata() {
+        snake.updata()
     }
 
     draw() {
-        this.canvas.draw.bind(canvas)
-        this.apply.draw(this.canvas.context)
+        canvas.draw()
+        apply.draw()
+        snake.draw()
     }
 }
+let canvas = new Canvas()
+let config = new Config()
+let apply = new Apply()
+let snake = new Snake()
 
 const game = new Game()
 
